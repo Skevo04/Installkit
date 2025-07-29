0e43f9eceb2155801f97f17699c773d0633c37ad
Skevo04 <markbalint01@gmail.com>
2025-07-29 14:55:59 +0300

    Kubernetes installation

--- install_commit.sh
+++ install_commit.sh
@@ -21,3 +21,11 @@ sudo apt install -y podman
 podman --version
 commit_changes "Installed Podman"
 
+# --- Kubernetes (kubectl) Installation ---
+echo "Installing kubectl..."
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+kubectl version --client
+commit_changes "Installed kubectl"
+
+echo "All tools installed and committed to Git!"
