0985030afbbdc501f1b074f6001ead49f6d71b55
Skevo04 <markbalint01@gmail.com>
2025-07-29 14:49:58 +0300

    Installed Docker

---
+++ install_commit.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Function to commit changes
+commit_changes() {
+    git add .
+    git commit -m "$1"
+    git push origin main
+}
+
+# --- Docker Installation ---
+echo "Installing Docker..."
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+sudo usermod -aG docker $USER
+docker --version
+commit_changes "Installed Docker"
