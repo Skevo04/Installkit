06b0a53ec927cc5e3f520aab4546d33d2bca5e95
Skevo04 <markbalint01@gmail.com>
2025-07-29 14:52:44 +0300

    Podman and Docker installation

--- install_commit.sh
+++ install_commit.sh
@@ -14,3 +14,10 @@ sudo sh get-docker.sh
 sudo usermod -aG docker $USER
 docker --version
 commit_changes "Installed Docker"
+
+# --- Podman Installation ---
+echo "Installing Podman..."
+sudo apt install -y podman
+podman --version
+commit_changes "Installed Podman"
+
